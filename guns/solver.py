088eaf42b14724300e5cb8ae5d5265efc293fe26
dancers <149972831+alluding@users.noreply.github.com>
2024-05-04 22:35:29 -0400

    fix logging multiple times

--- guns/solver.py
+++ guns/solver.py
@@ -17,20 +17,19 @@ class SolverConfig(BaseModel):
     useragent: Optional[str] = None
 
 
+logger: logging.Logger = logging.getLogger("Solver")
+logger.setLevel(logging.DEBUG)
+
+if (handler := logging.StreamHandler()) is not None:
+    handler.setFormatter(Logger())
+    logger.addHandler(handler)
+
+
 class Solver:
     BASE_URL: str = "https://api.fcaptcha.lol/api/"
 
     def __init__(self, config: SolverConfig) -> None:
         self.config: SolverConfig = config
-        self.config_logger()
-
-    def config_logger(self: Solver) -> None:
-        self.logger: logging.Logger = logging.getLogger("Solver")
-        self.logger.setLevel(logging.DEBUG)
-
-        if (handler := logging.StreamHandler()) is not None:
-            handler.setFormatter(Logger())
-            self.logger.addHandler(handler)
 
     def solve(self: Solver) -> str:
         try:
@@ -51,7 +50,7 @@ class Solver:
                 "Failed to get task ID!"
             )
             new_payload: Dict[str, str] = {"task_id": task_id}
-            self.logger.info(f"Retrieved task_id » {task_id}")
+            logger.info(f"Retrieved task_id » {task_id}")
 
             while True:
                 result = requests.get(
@@ -63,7 +62,8 @@ class Solver:
                 if result["task"].get("state") == "processing":
                     continue
 
-                self.logger.info(f"Successfully solved captcha » {result['task']['time']}")
+                logger.info(
+                    f"Successfully solved captcha » {result['task']['time']}")
                 return result["task"].get("captcha_key")
         except Exception as e:
-            self.logger.error(f"Failed to solve captcha » {e}")
+            logger.error(f"Failed to solve captcha » {e}")
